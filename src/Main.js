dbfaae0f9f3e5c02fe9a25035341cd824d55f2c7
Angela Branaes <angela.branaes@gmail.com>
2024-09-22 14:46:07 +0100

    updated background colour and some text to be ciaohello related

--- src/Main.js
+++ src/Main.js
@@ -4,13 +4,15 @@ import Screenshot from './static/images/screenshot.png'
 export default class Main extends Component {
   render() {
     return (
-      <div id="main">
+      <div id="main" style={{
+        minHeight: '100vh',
+        background: 'linear-gradient(to bottom, #FFA500, #D56AFA)'
+      }}>
         <header className="App-header">
           <div>
-          <h1>Remember to Slack your colleagues</h1>
+          <h1>Ciao Hello</h1>
           <h4>
-            Keep track of how many colleagues you've spoken to lately,
-            and who it's time to reach out to again.
+            A new way to learn a language coming soon...
           </h4>
           <br></br>
           <a href="https://slack.com/oauth/v2/authorize?client_id=1052632707317.1057131634261&scope=chat:write,files:write,users:read&user_scope=im:read"><img alt="Add to Slack" height="45" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"/></a>
@@ -19,11 +21,6 @@ export default class Main extends Component {
             <img className="screenshot" src={ Screenshot } alt="screenshot of Chatty app" />
           </div>
         </header>
-        <section className="logos">
-        </section>
-        <section className="boxes">
-          
-        </section>
       </div>
     )
   }
